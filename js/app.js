c0a343d24c48ab909f72be2932e9b1b2be6ba5be
Burk Price <webigrator@gmail.com>
2020-04-16 14:21:16 -0600

    app js

--- js/app.js
+++ js/app.js
@@ -1,62 +1,135 @@
 window.onload = function() {
-	const builder = cydran.builder;
-	const Component = cydran.Component;
-	const Stage = cydran.stage;
-	const StageBuilder = cydran.StageBuilder;
+  const builder = cydran.builder;
+  const Component = cydran.Component;
+  const Stage = cydran.stage;
+  const StageBuilder = cydran.StageBuilder;
 
-	const KEY_ENTER = 13;
-	const KEY_ESC = 27;
-	const APP_TEMPLATE = document.querySelector("template[id=app]").innerHTML.trim();
+  const KEY_ENTER = 13;
+  const KEY_ESC = 27;
+  const APP_TEMPLATE = document
+    .querySelector("template[id=app]")
+    .innerHTML.trim();
+  const TODO_TEMPLATE = document
+    .querySelector("template[id=todoitem]")
+    .innerHTML.trim();
 
-	class App extends Component {
+	class TodoItem {
+		constructor() {
+			this.title = null;
+			this.completed = false;
+		}
+	};
+
+  class App extends Component {
     constructor() {
       super(APP_TEMPLATE);
       this.log = this.getLogger();
+      this.watch("m().todos", () => {
+        this.remaining = this.todos.filter(t => !t.completed).length;
+      });
     }
 
     init() {
-			this.todos = [];
-			this.filteredTodos = [];
-			this.allDone = false;
-			this.editedTodo = null;
-			this.remaining = 0;
+      this.todos = [];
+      this.filterVisiblity = "all";
+      this.allDone = false;
+      this.newTodoValue = "";
     }
 
-		addTodo() {
-			// keyup enter
-		}
+    setFilter(filter) {
+      this.filterVisiblity = filter;
+      this.log.ifDebug(() => "filter set to " + this.filterVisiblity);
+    }
 
-		editTodo(todo) {
+    filteredTodos() {
+			let retval = [];
+      switch (this.filterVisiblity) {
+        case "active":
+          retval = this.todos.filter(t => (t.completed == false));
+          break;
+        case "completed":
+          retval = this.todos.filter(t => t.completed);
+          break;
+				default:
+					retval = this.todos;
+					break;
+      }
+			return retval;
+    }
 
-		}
+    addTodo(event) {
+      if (event.keyCode == KEY_ENTER) {
+        const newTodo = new TodoItem();
+				newTodo.title = this.newTodoValue;
+        this.todos.push(newTodo);
+				event.target.value = "";
+      	this.log.ifDebug(() => "new todo added: " + JSON.stringify(newTodo));
+      }
+    }
 
-		removeTodo(todo) {
+    removeTodo(todo) {
+      const removeIdx = this.todos.indexOf(todo);
+      this.todos.splice(removeIdx, 1);
+    }
 
-		}
-
-		finishEdit(todo) {
-			// keyup enter -> doneEdit
-			// keyup esc => cancelEdit
-		}
-
-		doneEdit(todo) {
-
-		}
-
-		cancelEdit(todo) {
-
-		}
-
-		removeCompleted() {
-
-		}
-
+    removeCompleted() {
+      if (this.todos) {
+        const purgedTodos = this.todos.filter(item => !item.completed);
+        this.todos = purgedTodos;
+      }
+    }
   }
 
-  builder("body")
+	class Todo extends Component {
+		constructor() {
+			super(TODO_TEMPLATE);
+		}
+
+		init() {
+			this.editMode = false;
+      this.origEditText = "";
+		}
+
+    edit() {
+			this.editMode = true;
+      this.origEditText = this.getItem().title;
+      this.log.ifDebug(() => "begin edit of todo" + JSON.stringify(this.getItem()));
+    }
+
+    cancelEdit() {
+      this.getItem().title = this.origEditText;
+      this.origEditText = "";
+			this.editMode = false;
+      this.log.ifDebug(() => "cancel edit of todo" + JSON.stringify(this.getItem()));
+    }
+
+    doneEdit() {
+      this.origEditText = "";
+			this.editMode = false;
+			this.log.ifDebug(() => "finish edit of todo");
+    }
+
+    finishEdit(event) {
+      switch (event.keyCode) {
+        case KEY_ENTER:
+          this.doneEdit();
+          break;
+        case KEY_ESC:
+          this.cancelEdit();
+          break;
+      }
+    }
+
+		toggleComplete() {
+			this.getItem().completed = !this.getItem().completed;
+		}
+	}
+
+  builder("#appbody")
     .withDebugLogging()
-		.withScopeItem("pluralize", (str, cnt) => (cnt > 1) ? (str + "s") : str)
+    .withScopeItem("pluralize", (str, cnt) => (cnt != 1 ? str + "s" : str))
     .withPrototype(App.name, App)
+		.withPrototype(Todo.name, Todo)
     .withInitializer(stage => {
       stage.setComponentFromRegistry(App.name);
     })
