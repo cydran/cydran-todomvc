52112caa749a29867a3c75f1913d0c9c30b113ae
Burk Price <webigrator@gmail.com>
2020-04-16 15:52:52 -0600

    more updates

--- js/app.js
+++ js/app.js
@@ -13,12 +13,13 @@ window.onload = function() {
     .querySelector("template[id=todoitem]")
     .innerHTML.trim();
 
-	class TodoItem {
-		constructor() {
-			this.title = null;
-			this.completed = false;
-		}
-	};
+  class TodoItem {
+    constructor() {
+      this.id = null;
+      this.title = null;
+      this.completed = false;
+    }
+  }
 
   class App extends Component {
     constructor() {
@@ -27,10 +28,12 @@ window.onload = function() {
       this.watch("m().todos", () => {
         this.remaining = this.todos.filter(t => !t.completed).length;
       });
+      this.watch("m().allDone", this.toggleAllComplete);
     }
 
     init() {
       this.todos = [];
+      this.remaining = this.todos.length;
       this.filterVisiblity = "all";
       this.allDone = false;
       this.newTodoValue = "";
@@ -42,28 +45,28 @@ window.onload = function() {
     }
 
     filteredTodos() {
-			let retval = [];
+      let retval = [];
       switch (this.filterVisiblity) {
         case "active":
-          retval = this.todos.filter(t => (t.completed == false));
+          retval = this.todos.filter(t => t.completed == false);
           break;
         case "completed":
           retval = this.todos.filter(t => t.completed);
           break;
-				default:
-					retval = this.todos;
-					break;
+        default:
+          retval = this.todos;
+          break;
       }
-			return retval;
+      return retval;
     }
 
     addTodo(event) {
       if (event.keyCode == KEY_ENTER) {
         const newTodo = new TodoItem();
-				newTodo.title = this.newTodoValue;
+        newTodo.title = this.newTodoValue;
         this.todos.push(newTodo);
-				event.target.value = "";
-      	this.log.ifDebug(() => "new todo added: " + JSON.stringify(newTodo));
+        this.log.ifDebug(() => "new todo added: " + JSON.stringify(newTodo));
+        event.target.value = "";
       }
     }
 
@@ -78,35 +81,55 @@ window.onload = function() {
         this.todos = purgedTodos;
       }
     }
+
+		completedCount() {
+			let retval = 0;
+			for (const t in this.todos) {
+				if (t.completed) {
+					++retval;
+				}
+			}
+		}
+
+    toggleAllComplete() {
+      this.todos.forEach(todo => {
+        todo.completed = this.allDone;
+      });
+			console.log(this.todos);
+    }
   }
 
-	class Todo extends Component {
-		constructor() {
-			super(TODO_TEMPLATE);
-		}
+  class Todo extends Component {
+    constructor() {
+      super(TODO_TEMPLATE);
+    }
 
-		init() {
-			this.editMode = false;
+    init() {
+      this.editMode = false;
       this.origEditText = "";
-		}
+    }
 
     edit() {
-			this.editMode = true;
+      this.editMode = true;
       this.origEditText = this.getItem().title;
-      this.log.ifDebug(() => "begin edit of todo" + JSON.stringify(this.getItem()));
+      this.log.ifDebug(
+        () => "begin edit of todo" + JSON.stringify(this.getItem())
+      );
     }
 
     cancelEdit() {
       this.getItem().title = this.origEditText;
       this.origEditText = "";
-			this.editMode = false;
-      this.log.ifDebug(() => "cancel edit of todo" + JSON.stringify(this.getItem()));
+      this.editMode = false;
+      this.log.ifDebug(
+        () => "cancel edit of todo" + JSON.stringify(this.getItem())
+      );
     }
 
     doneEdit() {
       this.origEditText = "";
-			this.editMode = false;
-			this.log.ifDebug(() => "finish edit of todo");
+      this.editMode = false;
+      this.log.ifDebug(() => "finish edit of todo");
     }
 
     finishEdit(event) {
@@ -120,16 +143,16 @@ window.onload = function() {
       }
     }
 
-		toggleComplete() {
-			this.getItem().completed = !this.getItem().completed;
-		}
-	}
+    toggleComplete() {
+      this.getItem().completed = !this.getItem().completed;
+    }
+  }
 
-  builder("#appbody")
+  builder("body>div#appbody")
     .withDebugLogging()
     .withScopeItem("pluralize", (str, cnt) => (cnt != 1 ? str + "s" : str))
     .withPrototype(App.name, App)
-		.withPrototype(Todo.name, Todo)
+    .withPrototype(Todo.name, Todo)
     .withInitializer(stage => {
       stage.setComponentFromRegistry(App.name);
     })
