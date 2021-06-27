5bb8fa1a885d4f32a7c12f71d9245ff8878e3be7
Burk Price <webigrator@gmail.com>
2021-06-27 13:47:05 -0600

    updated tarball script

--- tarball.sh
+++ tarball.sh
@@ -1,1 +1,1 @@
-tar czfvm cydran-todomvc.tgz css index.html js learn.json LICENSE node_modules README.md
+tar czfvm cydran-todomvc.tgz css index.html js learn.json LICENSE node_modules node_modules/cydran/dist/cydran.* README.md
