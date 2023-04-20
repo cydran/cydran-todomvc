d49adb77994e5f147e9a9556e9dd16eb8467622d
Burk Price <webigrator@gmail.com>
2023-04-19 22:37:46 -0600

    application image resources

--- tarball.sh
+++ tarball.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tar czfm cydran-todomvc.tgz css index.html js learn.json LICENSE README.md node_modules/todomvc* node_modules/cydran/dist/cydran.*
+tar czfm cydran-todomvc.tgz index.html css js images robots.txt manifest.json learn.json LICENSE README.md node_modules/todomvc* node_modules/cydran/dist/cydran.*
 
 pubdir="./public"
 
