d3653a3ca5569535e9b1f9e21319e487011bf931
Burk Price <evilchia@gmail.com>
2023-06-10 16:28:40 -0600

    rebuild the repo to localstorage

--- js/repo.js
+++ js/repo.js
@@ -4,24 +4,44 @@ const defaultState = "all";
 
 class TodoRepo {
 	constructor(logger) {
+		this.repo = window.localStorage;
 		this.logr = logger;
 	}
 
-	storeAll(todos) {
-		this.logr.ifTrace(() => `store all: ${ JSON.stringify(todos) }`);
-		window.localStorage.setItem(listName, JSON.stringify(todos));
+	add(todo) {
+		this.logr.ifTrace(() => `add todo: id = ${ todo.id }`);
+		this.repo.setItem(todo.id, JSON.stringify(todo));
 	}
+
+	update(todo) {
+		this.logr.ifTrace(() => `update todo: id = ${ todo.id }`);
+		this.repo.setItem(todo.id, JSON.stringify(todo));
+	}
+
+	remove(todo) {
+		this.logr.ifTrace(() => `remove todo: id = ${ todo.id }`);
+		this.repo.removeItem(todo.id);
+	}
+
 	getAll() {
 		this.logr.ifTrace(() => `get all todos`);
-		return JSON.parse(window.localStorage.getItem(listName)) || [];
+		const retval = [];
+		Object.keys(this.repo).forEach(k => {
+			if(k !== visibilityState) {
+				retval.push(JSON.parse(this.repo.getItem(k)));
+			}
+		});
+		return retval;
 	}
+
 	storeVisibleState(state) {
 		this.logr.ifTrace(() => `store visible state: ${ state }`);
-		window.localStorage.setItem(visibilityState, state);
+		this.repo.setItem(visibilityState, state);
 	}
+
 	getVisibleState() {
 		this.logr.ifTrace(() => `get visible state`);
-		return window.localStorage.getItem(visibilityState) || defaultState;
+		return this.repo.getItem(visibilityState) || defaultState;
 	}
 }
 
