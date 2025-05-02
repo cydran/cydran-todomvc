8ddd9ce9127a012d15bf44801da3975724b50d50
Burk Price <evilchia@gmail.com>
2025-05-01 22:16:03 -0600

    using @cydran/cydran 0.4.0

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
-import { TodoRepo, MSG } from "./data/repo.js";
-import { TodoEntity } from "./data/TodoEntity.js";
+import { TodoRepo, MSG } from './data/repo.js';
+import { TodoEntity } from './data/TodoEntity.js';
 
-// import { quotes } from "./data/quotes.js";
+// import quotes from "./data/quotes.json" with {type: 'json'};
 /*
 import {argumentsBuilder as ab, PropertyKeys, uuidV4, Component} from "../node_modules/cydran/dist/cydran.js";
 */
@@ -10,67 +10,77 @@ const ab = cydran.argumentsBuilder;
 const Component = cydran.Component;
 const create = cydran.create;
 const PropertyKeys = cydran.PropertyKeys;
+const To = cydran.To;
 
-const KEY_ENTER = "Enter";
-const KEY_ESC = "Escape";
-const TODO_CHANNEL = "TODOS";
-const RMV_TODO = "removeTodo";
-const UP_TODO = "updateTodo";
-const EMPTY_STR = "";
+const KEY_ENTER = 'Enter';
+const KEY_ESC = 'Escape';
+const EMPTY_STR = '';
 
-const DATA_SRLZ_LVL = "data.serialize.level";
-const PERSONALIZED = "todo.person";
+const TODO_CHANNEL = 'TODOS';
+const RMV_TODO = 'removeTodo';
+const PATCH_TODO = 'patchTodo';
+
+const DATA_SRLZ_LVL = 'cydran.logging.TodoRepo.level';
+const PERSONALIZED = 'todo.person';
 
 const PROPERTIES = {
 	[PERSONALIZED]: EMPTY_STR,
 	[PropertyKeys.CYDRAN_STRICT_ENABLED]: true,
-	[PropertyKeys.CYDRAN_STRICT_STARTPHRASE]: "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday's code. (Dan Salomon)",
-	[`${ PropertyKeys.CYDRAN_LOG_COLOR_PREFIX }.debug`]: "#00f900",
- 	[PropertyKeys.CYDRAN_LOG_LEVEL]: "DEBUG",
-	[PropertyKeys.CYDRAN_LOG_LABEL]: "ctdmvc",
+	[PropertyKeys.CYDRAN_STRICT_STARTPHRASE]:
+		"Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday's code. (Dan Salomon)",
+	[`${PropertyKeys.CYDRAN_LOG_COLOR_PREFIX}.debug`]: '#00f900',
+	[PropertyKeys.CYDRAN_LOG_LEVEL]: 'trace',
+	[PropertyKeys.CYDRAN_LOG_LABEL]: 'ctdmvc',
 	[PropertyKeys.CYDRAN_LOG_LABEL_VISIBLE]: false,
-	[PropertyKeys.CYDRAN_LOG_PREAMBLE_ORDER]: "level:name",
-	[DATA_SRLZ_LVL]: "TRACE"
+	[PropertyKeys.CYDRAN_LOG_PREAMBLE_ORDER]: 'level:name',
+	[DATA_SRLZ_LVL]: 'trace'
 };
 
-const template = (id) => document.querySelector(`template[id=${id}]`).innerHTML.trim();
+const template = id =>
+	document.querySelector(`template[id=${id}]`).innerHTML.trim();
 
 class App extends Component {
-	constructor(who, newIds) {
+	constructor() {
 		super(template(App.name.toLowerCase()));
 
-		this.who = who || EMPTY_STR;
-		this.newIds = newIds;
+		this.filterVisiblity;
 		this.todos = [];
-		this.$c().onExpressionValueChange("m().todos", () => {
-			this.computeRemaining();
-		});
+		this.$c().onExpressionValueChange('m().todos', () =>
+			this.computeRemaining()
+		);
+		this.$c().onExpressionValueChange('m().filterVisiblity', () =>
+			this.repo.storeVisibleState(this.filterVisiblity)
+		);
 
 		this.remaining = 0;
 		this.togAllDoneOrNot = false;
 		this.newTodoValue = EMPTY_STR;
 
-		this.$c().onExpressionValueChange("m().filterVisiblity", () => this.repo.storeVisibleState(this.filterVisiblity));
-
-		this.$c().onMessage(RMV_TODO).forChannel(TODO_CHANNEL).invoke(this.removeTodo);
-
-		this.$c().onMessage(UP_TODO).forChannel(TODO_CHANNEL).invoke(this.updateTodo);
-
-		this.$c().onMessage(MSG.ALL).forChannel(MSG.CHAN).invoke((data) => {
-			this.todos = data;
+		// msgs from todo items
+		this.$c().onMessage(RMV_TODO).forChannel(TODO_CHANNEL).invoke((payload) => {
+			this.$c().getLogger(`Removing a todo item: ${payload.id}`);
+			this.removeTodo(payload);
+		});
+		this.$c().onMessage(PATCH_TODO).forChannel(TODO_CHANNEL).invoke((payload) => {
+			this.$c().getLogger(`Updating a todo item: ${payload.id}`)
+			this.updateTodo(payload);
 		});
 
-		this.$c().onMessage(MSG.GS).forChannel(MSG.CHAN).invoke((data) => {
-			this.filterVisiblity = data;
-		});
+		// msgs from the repo
+		this.$c().onMessage(MSG.ALL).forChannel(MSG.CHAN).invoke(data => (this.todos = data));
+		this.$c().onMessage(MSG.STATE).forChannel(MSG.CHAN).invoke(data => (this.filterVisiblity = data));
 	}
 
 	onMount() {
 		this.repo = this.$c().getObject(TodoRepo.name);
 		this.repo.getVisibleState();
 		this.repo.getAll();
-		this.filtered = this.$c().createFilter("m().todos")
-			.withPredicate("p(0) === 'all' || !v().completed && p(0) === 'active' || v().completed && p(0) === 'completed'", "m().filterVisiblity")
+		this.filtered = this.$c()
+			.createFilter('m().todos')
+			.withPredicate(
+				"p(0) === 'all' || !v().completed && p(0) === 'active' || v().completed && p(0) === 'completed'",
+				'm().filterVisiblity'
+			)
 			.build();
 	}
 
@@ -80,7 +90,7 @@ class App extends Component {
 
 	addTodo(event) {
 		if (event.code === KEY_ENTER) {
-			let newTodoItem = this.getContext().get(TodoEntity.name);
+			const newTodoItem = new TodoEntity(self.crypto.randomUUID());
 			newTodoItem.title = this.newTodoValue;
 			event.target.value = EMPTY_STR;
 			this.todos.push(newTodoItem);
@@ -103,7 +113,8 @@ class App extends Component {
 	}
 
 	removeCompletedItems() {
-		this.todos.filter(item => item.completed)
+		this.todos
+			.filter(item => item.completed)
 			.forEach(itm => {
 				this.repo.remove(itm);
 			});
@@ -111,9 +122,11 @@ class App extends Component {
 	}
 
 	toggleAll() {
-		this.todos.forEach(todo => todo.completed = !this.togAllDoneOrNot);
+		this.todos.forEach(todo => (todo.completed = !this.togAllDoneOrNot));
 		this.togAllDoneOrNot = !this.togAllDoneOrNot;
-		this.$c().getLogger().ifDebug(() => `all items marked done: ${this.togAllDoneOrNot}`);
+		this.$c()
+			.getLogger()
+			.ifDebug(() => `all items marked done: ${this.togAllDoneOrNot}`);
 	}
 }
 
@@ -124,15 +137,13 @@ class TodoItem extends Component {
 		this.origEditText = EMPTY_STR;
 		this.dirty = false;
 
-		this.$c().onExpressionValueChange("v().completed", () => {
-			this.$c().send(UP_TODO, this.$c().getValue()).onChannel(TODO_CHANNEL).toContext();
+		this.$c().onExpressionValueChange('v().completed', () => {
+			this.$c().send(To.GLOBALLY, TODO_CHANNEL, PATCH_TODO, this.$c().getValue());
 		});
 	}
 
-	kill(event) {
-		if (event.detail === 1) {
-			this.$c().send(RMV_TODO, this.$c().getValue()).onChannel(TODO_CHANNEL).toContext();
-		}
+	kill() {
+		this.$c().send(To.GLOBALLY, TODO_CHANNEL, RMV_TODO, this.$c().getValue());
 	}
 
 	edit() {
@@ -144,7 +155,7 @@ class TodoItem extends Component {
 		if (event.code == KEY_ENTER) {
 			this.inEditMode = !this.inEditMode;
 			this.origEditText = EMPTY_STR;
-			this.$c().send(UP_TODO, this.$c().getValue()).onChannel(TODO_CHANNEL).toContext();
+			this.$c().send(To.GLOBALLY, TODO_CHANNEL, PATCH_TODO, this.$c().getValue());
 		}
 	}
 
@@ -154,25 +165,26 @@ class TodoItem extends Component {
 }
 
 function rootCapability(ctxt) {
-	ctxt.getScope().add("pluralize", (str, cnt) => (cnt !== 1 ? `${str}s` : str));
-	ctxt.registerPrototype(TodoEntity.name, TodoEntity, ab().withFunction(self.crypto.randomUUID))
-	ctxt.registerSingleton(TodoRepo.name, TodoRepo, ab()
-		.withLogger(`${App.name}.Repo`, ctxt.getProperties().getAsString(DATA_SRLZ_LVL))
-		.withPubSub()
-		.build()
+	ctxt.getScope().add('pluralize', (str, cnt) => (cnt !== 1 ? `${str}s` : str));
+	ctxt.registerPrototype(App.name, App);
+	ctxt.registerSingleton(
+		TodoRepo.name,
+		TodoRepo,
+		ab()
+			.withLogger(
+				`${App.name}.Repo`,
+				ctxt.getProperties().getAsString(DATA_SRLZ_LVL)
+			)
+			.withTransmitter()
+			.build()
 	);
 	ctxt.registerPrototype(TodoItem.name, TodoItem);
 }
 
-quotes.forEach(q => {
-	console.log(`${q.quote}\n\t${q.source}`);
-});
-
-const stage = create("body>div#appbody", PROPERTIES);
-stage.addInitializer(stage => {
-	stage.getContext().configure(rootCapability);
-	stage.setComponent(new App(PROPERTIES[PERSONALIZED] ?? EMPTY_STR, 11));
+const stage = create('body>div#appbody', PROPERTIES, rootCapability);
+stage.addInitializer(null, s => {
+	s.setComponentByObjectId('App');
 });
 
 stage.start();
-window["stage"] = stage;
+window['stage'] = stage;
--- src/js/data/repo.js
+++ src/js/data/repo.js
@@ -1,4 +1,6 @@
-import { Dexie } from "./dexie.mjs";
+import { Dexie } from "/js/data/dexie.mjs";
+
+const To = cydran.To;
 
 const DB_NAME = "todolist-dexie";
 const TBL_TD = "todos";
@@ -14,12 +16,12 @@ const DEF_STATE = "all";
 export const MSG = {
 	CHAN: "DATA",
 	ALL: "GetAll",
-	GS: "GetState"
+	STATE: "ModState"
 }
 
 export class TodoRepo {
-	constructor(logger, pubsub) {
-		this.ps =  pubsub;
+	constructor(logger, trxmtr) {
+		this.trxmtr = trxmtr;
 		this.logr = logger;
 		this.db = new Dexie(DB_NAME, { autoOpen: false });
 		this.db.version(1).stores(SCHEMA);
@@ -38,7 +40,7 @@ export class TodoRepo {
 
 	update(todo) {
 		this.db[TBL_TD].put(todo).then(async resp => {
-			await this.logr.ifTrace(() => `update todo: id = ${ resp }`);
+			await this.logr.ifTrace(() => `update todo: id = ${ todo.id }`);
 		}).catch(err => {
 			this.logr.ifError(() => err);
 		});
@@ -54,16 +56,16 @@ export class TodoRepo {
 
 	getAll() {
 		this.db[TBL_TD].toArray().then(async resp => {
-			await this.ps.sendGlobally(MSG.CHAN, MSG.ALL, resp);
 			this.logr.ifTrace(() => `get all todos`);
+			await this.trxmtr.send(To.GLOBALLY, MSG.CHAN, MSG.ALL, resp);
 		}).catch(err => {
-			this.ps.sendToContext(MSG.CHAN, MSG.ALL, []);
+			this.trxmtr.send(To.GLOBALLY, MSG.CHAN, MSG.ALL, []);
 		});
 	}
 
 	storeVisibleState(state) {
 		this.db[TBL_TS].put({"id": 1, "value": state}).then(resp => {
-			this.logr.ifTrace(() => `add/update visibile state`);
+			this.logr.ifTrace(() => `add/update visibile state: ${state}`);
 		}).catch(err => {
 			this.logr.ifError(() => err);
 		});
@@ -71,10 +73,10 @@ export class TodoRepo {
 
 	getVisibleState() {
 		this.db[TBL_TS].get(1).then(async resp => {
-			await this.ps.sendGlobally(MSG.CHAN, MSG.GS, resp.value);
+			await this.trxmtr.send(To.GLOBALLY, MSG.CHAN, MSG.STATE, resp.value);
 			this.logr.ifTrace(() => `get visible state`);
 		}).catch(err => {
-			this.ps.sendToContext(MSG.CHAN, MSG.GS, DEF_STATE);
+			this.trxmtr.send(To.GLOBALLY, MSG.CHAN, MSG.STATE, DEF_STATE);
 		});
 	}
 }
